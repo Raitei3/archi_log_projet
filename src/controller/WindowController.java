3823a69269cd1f56be95f8a42fac701c813711fa
Antoine Borde <antoine24.borde@gmail.com>
2017-04-25 15:05:03 +0200

    test dessin

--- src/controller/WindowController.java
+++ src/controller/WindowController.java
@@ -1,10 +1,16 @@
 package controller;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
+import java.awt.Point;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import model.Rect;
+import model.RectJavaFX;
+import model.Shape;
 
 public class WindowController implements Initializable {
 
@@ -24,6 +30,10 @@ public class WindowController implements Initializable {
 		@FXML
 		private Button trash;
 
+		@FXML
+		private AnchorPane dashboard;
+		
+		private ArrayList<Shape> dashboardShape;
 		
 		
 		public WindowController(){}
@@ -31,6 +41,8 @@ public class WindowController implements Initializable {
 		@Override
 		public void initialize(URL arg0, ResourceBundle arg1) {
 			System.out.println("test");
+			
+			initDashboard();
 			
 			save.setOnAction((event)-> {
 				System.out.println("button save");
@@ -52,5 +64,12 @@ public class WindowController implements Initializable {
 				System.out.println("button trash");
 				});
 
+		}
+		
+		public void initDashboard(){
+			
+			Rect r = new Rect(new Point(20,20),20,20);
+			System.out.println("tamere2");
+			RectJavaFX rx = new RectJavaFX(r);
 		}
 }
--- src/test/Main.java
+++ src/test/Main.java
@@ -7,7 +7,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -18,50 +17,18 @@ public class Main extends Application {
 
 	@Override
 	public void start(Stage primaryStage) {
-	primaryStage.setTitle("Projet AL");
+		primaryStage.setTitle("Projet AL");
 	
-	try {
-		FXMLLoader loader = new FXMLLoader(Main.class.getResource("../view/PersonOverview.fxml"));
-		AnchorPane page = (AnchorPane) loader.load();
-		WindowController controller = (WindowController) loader.getController();
-		Scene scene = new Scene(page);
-		primaryStage.setScene(scene);
-		primaryStage.show();
-	} catch (IOException e) {
-		e.printStackTrace();
-	}
-}
-	
-	//initRootLayout();
-	//showPersonOverview();
-
-	/*public void initRootLayout(){
 		try {
-			FXMLLoader fxml = new FXMLLoader();
-			fxml.setLocation(MainApp.class.getResource("Window.fxml"));
-			root = (BorderPane) fxml.load();
-
-			Scene s = new Scene(root);
-			//stage.setScene(s);
-			//stage.show();
-		}catch(IOException e){
+			FXMLLoader loader = new FXMLLoader(Main.class.getResource("../view/PersonOverview.fxml"));
+			AnchorPane page = (AnchorPane) loader.load();
+			Scene scene = new Scene(page);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
-	}
-
-	public void showPersonOverview(){
-		try{
-			FXMLLoader fxml2 = new FXMLLoader();
-			fxml2.setLocation(MainApp.class.getResource("PersonOverview.fxml"));
-			AnchorPane personOverview = (AnchorPane) fxml2.load();
-			root.setCenter(personOverview);
-		}catch(IOException e){
-			e.printStackTrace();
-		}
-	}*/
-
-
-
+	}	
 	public static void main(String[] args) {
 		launch(args);
 	}
